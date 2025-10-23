a89ed6da123e7effe555ab33992e133a438dc2ff
Eduard_Tokos <xtokose@stuba.sk>
2025-10-23 21:59:49 +0200

    change

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,7 @@ struct BuildingPoint {
     int intensity;
     int classification;
     double nx = 0, ny = 0, nz = 0;
+    double curvature;
 };
 
 
@@ -65,36 +66,31 @@ int main() {
     _putenv_s("GDAL_DATA", "C:\\vcpkg\\installed\\x64-windows\\share\\gdal");
     auto start = std::chrono::high_resolution_clock::now();
 
-    std::cout << "test";
 
-    pdal::StageFactory factory;     ////stage
+    pdal::StageFactory factory;         ////stage
 
 
-    //reader
+        //reader
     pdal::Stage* reader = factory.createStage("readers.las");
-    pdal::Options opts;     ////options
-    opts.add("filename", R"(LiDAR.laz)"); // <-- .laz file
+    pdal::Options opts;                 ////options
+    opts.add("filename", R"(LiDAR.laz)");
     reader->setOptions(opts);
 
-    //filtering class 6
+        //filtering class 6
     pdal::Stage* range = factory.createStage("filters.range");
     pdal::Options rng;
     rng.add("limits", "Classification[6:6]");
     range->setOptions(rng);
     range->setInput(*reader);
 
-    // Normal filter
+        // Normal filter
     pdal::Stage* normal = factory.createStage("filters.normal");
     pdal::Options no;
-    no.add("knn", 16);                         // number of neighbors (default 8)
-    // Optional:
-    // no.add("always_up", true);              // flip toward +Z (default true)
-    // no.add("refine", true);                 // MST propagation for more consistency
-    // no.add("viewpoint", "POINT Z (0 0 10)");// flip toward a 3D viewpoint (WKT/GeoJSON)
+    no.add("knn", 16);                         // number of neighbors
     normal->setOptions(no);
-    normal->setInput(*range); /////////input
+    normal->setInput(*range);        /////////input
 
-    //execute
+        //execute
     pdal::PointTable table;
     normal->prepare(table);
     reader->prepare(table);
@@ -102,18 +98,26 @@ int main() {
     pdal::PointViewSet views_all = reader->execute(table);
 
 
-    //printg total points
+        //printg total points
     std::size_t total2 = 0;
     for (auto v : views_all) total2 += v->size();
     std::cout << "Total points (LAZ): " << total2 << "\n";
+    for (auto const& v : views) {
+        std::cout << "Class-6 points: " << v->size() << "\n";
+    }
 
 
-    std::ofstream file("points.txt");
 
+    
 
-    // count normals (only class-6 points exist now)
+    
+    
+
+        // count normals (only class-6 points)
     std::vector<BuildingPoint> buildpoint;
-    std::size_t total = 0, ok = 0;
+    std::size_t total = 0;
+    std::ofstream file("points.txt");
+    file << std::fixed << std::setprecision(3);
     for (auto const& v : views)
     {
         total += v->size();
@@ -126,69 +130,37 @@ int main() {
             double nx = v->getFieldAs<double>(pdal::Dimension::Id::NormalX, i);
             double ny = v->getFieldAs<double>(pdal::Dimension::Id::NormalY, i);
             double nz = v->getFieldAs<double>(pdal::Dimension::Id::NormalZ, i);
+            double curv = v->getFieldAs<double>(pdal::Dimension::Id::Curvature, i);
             a.x = x;
             a.y = y;
             a.z = z;
             a.nx = nx;
             a.ny = ny;
             a.nz = nz;
+            a.curvature = curv;
 
-            file << x << "," << y << "," << z << endl;
+            file << a.x << "," << a.y << "," << a.z << std::endl;
+
 
             double ratio = 0.01;
             if (nx > ratio) {
                 buildpoint.push_back(a);
             }
 
-
-            //std::cout << "\npoint " << i << std::endl;
-           // std::cout << "nx " << nx << std::endl;
-
-            const double len2 = nx * nx + ny * ny + nz * nz;
-            if (std::isfinite(nx) && std::isfinite(ny) && std::isfinite(nz) && len2 > 0.0) ++ok;
         }
     }
     file.close();
 
-    //std::cout << "Class-6 points: " << total << "\n"
-    //    << "Class-6 normals valid: " << ok << "\n";
+    std::cout << "Class-6 points after filtering null normals in x direction: " << buildpoint.size() << "\n\n\n";
 
 
-    std::cout << " " << buildpoint.size() << std::endl;
-    for (int i = 0; i < 100; i++) {
-        std::cout << "point " << i << " ---    x " << buildpoint[i].x << std::endl;
-        std::cout << " nx " << buildpoint[i].nx << endl << endl;
+        //printing first 10 points
+    for (int i = 0; i < 10; i++) {
+        std::cout << "point " << i << endl;
+        std::cout << "x: " << buildpoint[i].x << "   y: " << buildpoint[i].y << "   z: " << buildpoint[i].z << endl;
+        std::cout << "nx: " << buildpoint[i].nx << "   ny: " << buildpoint[i].ny << "   nz: " << buildpoint[i].nz << endl;
+        std::cout << "curvature " << buildpoint[i].curvature << endl << endl;
 
-    }
-
-
-
-    //loading all points from .las
-    {
-        ///////////
-
-    //pdal::StageFactory factory;
-
-    //auto reader = factory.createStage("readers.las");
-    //pdal::Options opts;
-    //opts.add("filename", "Mracno_bodov_vyrez.las");   // put your LAS path here
-    //reader->setOptions(opts);
-
-
-
-
-    //pdal::PointTable table;
-    //reader->prepare(table);     //metadata    
-    //pdal::PointViewSet views = reader->execute(table);  //reading data
-
-
-    ////suma
-    //std::size_t total = 0;
-    //for (auto v : views) total += v->size();
-    //std::cout << "Total points: " << total << "\n";
-
-
-    /////////////////
     }
 
 
@@ -198,5 +170,5 @@ int main() {
 
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = end - start;
-    std::cout << "Program ran in " << elapsed.count() << " seconds.\n";
+    std::cout << "\n\nProgram ran in " << elapsed.count() << " seconds.\n";
 }
